86f037841ce5633b9347c52df91e798e1e190e35
Samuel Parkinson <51677+sjparkinson@users.noreply.github.com>
2018-11-01 22:59:39 +0000

    Don't continue if you guess correctly.

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,20 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
 
         println!("You guessed {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
